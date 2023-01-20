ec7ca3c0987df1b89fd4ef790b2108166a0c7ef2
fabio-alfieri <fabio.alfieri@ieo.it>
2023-01-20 15:09:00 +0100

    changes

--- 01_binLevelAnalysis_PCAWG.R
+++ 01_binLevelAnalysis_PCAWG.R
@@ -15,3 +15,22 @@ suppressMessages({
   library(parallel)
 })
 
+scratchMS <- "/home/ieo5099/mountHPC/scratch/MS/falfieri/PCAWG/"
+# columns <- read.table("/home/ieo5099/mountHPC/scratch/MS/falfieri/PCAWG/patient_per_tumortype.tsv",
+#                       skip = 1)
+
+tumor_type <- "PACA-CA"
+
+snv <- read.table(file = paste0(scratchMS,"snv/",tumor_type,"_snv.tsv"))
+cna <- read.table(file = paste0(scratchMS,"cna/",tumor_type,"_cna.tsv"))
+
+chr_info <-
+  read.table("/home/ieo5099/Desktop_linux/mutation_compensation/data/misc/chr_info_h19.txt", header = TRUE)
+chr_arms <-
+  read.table(file = "/home/ieo5099/Desktop_linux/mutation_compensation/data/misc/cytoBand.txt", header = T)
+chr_arms[, 2:3] <-
+  apply(chr_arms[, 2:3] / fixed_bin_length, 2, as.integer)
+
+for(chr in paste0("chr",1:22)){
+  
+}
